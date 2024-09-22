3e58c2e156b131733863365a95bd24ef9b5d3de3
Eachan Johnson <eachan.johnson@crick.ac.uk>
2024-09-22 16:41:57 +0100

    Tidy test script

--- test/scripts/test.sh
+++ test/scripts/test.sh
@@ -15,7 +15,7 @@ echo "${INPUTB[@]}" | tr ' ' $'\n' > $FILE2
 yunta dca-single $INPUT1 -2 $INPUT2 -o test/outputs/dca-single.tsv --plot test/outputs/dca-single --apc \
 && touch test/outputs/dca-single.success
 
-yunta dca-many $INPUTA -2 "${INPUTB[@]}" --apc -o test/outputs/dca-many.tsv --plot test/outputs/dca-many \
+yunta dca-many $INPUT1 -2 "${INPUTB[@]}" --apc -o test/outputs/dca-many.tsv --plot test/outputs/dca-many \
 && touch test/outputs/dca-many.success
 
 yunta dca-single $FILE1 -2 $FILE2 --apc -o test/outputs/dca-many-list.tsv --plot test/outputs/dca-many-list --list-file \
@@ -37,7 +37,7 @@ if [ -z $1 ]
 then
     yunta rf2t-single $INPUT1 -2 $INPUT2 -o test/outputs/rf2t-single.tsv --plot test/outputs/rf2t-single \
     && touch test/outputs/rf2t-single.success
-    yunta rf2t-single $INPUTA -2 "${INPUTB[@]}" -o test/outputs/rf2t-many.tsv --plot test/outputs/rf2t-many \
+    yunta rf2t-single $INPUT1 -2 "${INPUTB[@]}" -o test/outputs/rf2t-many.tsv --plot test/outputs/rf2t-many \
     && touch test/outputs/rf2t-many.success
     yunta rf2t-single $FILE1 -2 $FILE2 -o test/outputs/rf2t-many-list.tsv --plot test/outputs/rf2t-many-list --list-file \
     && touch test/outputs/rf2t-many-list.success
@@ -47,7 +47,7 @@ then
         exit 1
     fi
 
-    yunta af2-single $INPUTA -2 "${INPUTB[@]}" -o test/outputs/af2-many \
+    yunta af2-single $INPUT1 -2 "${INPUTB[@]}" -o test/outputs/af2-many \
     && touch test/outputs/af2-many.success
     yunta af2-single $FILE1 -2 $FILE2 -o test/outputs/af2-many-list --list-file \
     && touch test/outputs/af2-many-list.success
